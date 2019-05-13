40b2d7d811360cc4750f2a240934eb06801cefa6
Luckyfans <497620104@qq.com>
2019-05-13 23:30:17 +0800

    2

--- app/src/main/java/com/example/animation/Ball.java
+++ app/src/main/java/com/example/animation/Ball.java
@@ -23,7 +23,7 @@ public class Ball extends View {
     Paint mCirclePaint,mArcPaint,mTextPaint;
     float x,y,r,length,mShowSize;
     RectF rectF;
-    String mShowText=00+"%";
+    String mShowText=000+"%";
     public Ball(Context context) {
         this(context, null);
     }
